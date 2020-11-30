2ede271640656671658e9c129020f719a749ea78
Brent Gorwin <brent.gorwin@gmail.com>
2020-11-30 16:44:08 -0600

    Added React Router to the project and fixed the Nav Links to go to the correct pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 // Import Pages
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+
+// Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
+			<Nav />
 			<GlobalStyle />
-			<AboutUs />
+			<Switch>
+				<Route path='/' exact>
+					<AboutUs />
+				</Route>
+
+				<Route path='/work'>
+					<OurWork />
+				</Route>
+				<Route path='/contact'>
+					<ContactUs />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
