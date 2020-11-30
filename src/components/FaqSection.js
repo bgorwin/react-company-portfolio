f13539b3567045be053c06ecc85229854a4e03b1
Brent Gorwin <brent.gorwin@gmail.com>
2020-11-30 08:31:39 -0600

    Added styles to the FaqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Styles
+import styled from 'styled-components';
+import { About } from '../styles';
+
 const FaqSection = () => {
 	return (
-		<div className='faq'>
+		<Faq>
 			<h2>
 				Any Questions <span>FAQ</span>
 			</h2>
@@ -12,6 +16,7 @@ const FaqSection = () => {
 					<p>Lorem ipsum dolor sit amet.</p>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 			<div className='question'>
 				<h4>Daily Schedule</h4>
@@ -19,6 +24,7 @@ const FaqSection = () => {
 					<p>Lorem ipsum dolor sit amet.</p>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 			<div className='question'>
 				<h4>Different Payment Methods</h4>
@@ -26,6 +32,7 @@ const FaqSection = () => {
 					<p>Lorem ipsum dolor sit amet.</p>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 			<div className='question'>
 				<h4>What products do you offer?</h4>
@@ -33,9 +40,41 @@ const FaqSection = () => {
 					<p>Lorem ipsum dolor sit amet.</p>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
-		</div>
+		</Faq>
 	);
 };
+
+const Faq = styled(About)`
+	display: block;
+	span {
+		display: block;
+	}
+
+	h2 {
+		padding-bottom: 2rem;
+		font-weight: lighter;
+	}
+
+	.faq-line {
+		background: #cccccc;
+		height: 0.2rem;
+		margin: 2rem 0rem;
+		width: 100%;
+	}
+
+	.question {
+		padding: 3rem 0rem;
+		cursor: pointer;
+	}
+
+	.answer {
+		padding: 2rem 0rem;
+		p {
+			padding: 1rem 0rem;
+		}
+	}
+`;
 
 export default FaqSection;
