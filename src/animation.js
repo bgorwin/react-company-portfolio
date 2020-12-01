b7d8c20cb9f5b60c3f70c07f7e4bed34eede58c2
Brent Gorwin <brent.gorwin@gmail.com>
2020-12-01 08:30:21 -0600

    Added in Framer Motion animations and routing for all pages

---
+++ src/animation.js
@@ -0,0 +1,22 @@
+export const pageAnimation = {
+	hidden: {
+		opactiy: 0,
+		y: 300
+	},
+
+	show: {
+		opactiy: 1,
+		y: 0,
+		transition: {
+			duration: 1
+		}
+	},
+
+	exit: {
+		opactiy: 0,
+		y: 300,
+		transition: {
+			duration: 1
+		}
+	}
+};
